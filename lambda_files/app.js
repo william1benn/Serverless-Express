d27605f1e357dd214f224725f9f767e04743cdbb
William B <william.benn11@gmail.com>
2019-09-13 17:50:33 -0400

    updated original commit

--- lambda_files/app.js
+++ lambda_files/app.js
@@ -4,11 +4,12 @@ const sls = require('serverless-http');
 let cw = require('crypto-wallets');
 const app = express();
 
-
+//Put correct cors before upload to server 
 app.get('/*',function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   next(); 
 });
+
 
 app.get('/makeWallet',(req,res,next)=>{
   let btcWallet = cw.generateWallet('BTC');
